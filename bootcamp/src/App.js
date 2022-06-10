13fd032c4f9c63b48c4ef10cc530a7ffec4c1f13
Dickson Eneh <dickseneh99@gmail>
2022-06-10 05:07:21 +0200

    master

--- bootcamp/src/App.js
+++ bootcamp/src/App.js
@@ -1,6 +1,7 @@
 import Header from './componenets/Header/Header'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./componenets/Body/Home"
+import CreateRice from './componenets/createproduct/CreateRice'
 const  App=() =>{
   return (
    <BrowserRouter>
@@ -8,6 +9,7 @@ const  App=() =>{
 
    <Routes>
 <Route path= "/" element ={<Home/>}/>
+<Route path ="/createrice" element={<CreateRice/>}/>
    </Routes>
 
    </BrowserRouter>
